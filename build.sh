8ee04817637e2fa912931a5f02ef86485332389e
k7212519 <guys141211@gmail.com>
2025-11-29 01:59:18 +0800

    初始页样式修改

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ cp node_modules/vcd-stream/out/vcd.wasm app
 cp src/vcdrom.html app/index.html
 cp src/*.woff2 app
 cp src/theme.css app
+cp src/wave.svg app
 
 ./node_modules/.bin/browserify ./lib/vcdrom.js | ./node_modules/.bin/terser --compress -o app/vcdrom.js
